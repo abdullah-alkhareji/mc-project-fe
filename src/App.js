6755b78be5bbcff713a1b499c3d654addcc3a397
abdullah-alkhareji <outsiderandco@gmail.com>
2022-04-18 11:50:14 +0300

    clean the code

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import { Route, Routes } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import Home from "./components/Home/Home";
-import authStore from "./stores/authStore";
 
 const App = () => {
-  // if (!authStore.user) return <Login />;
   return (
     <Routes>
       <Route path="/" element={<Home />} />
