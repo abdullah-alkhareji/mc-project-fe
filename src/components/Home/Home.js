8369e19cbc3d5c3f9cc5c70fe9d9bdac7512f9ca
abdullah-alkhareji <outsiderandco@gmail.com>
2022-04-19 10:52:09 +0300

    fix home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,25 +1,22 @@
 import React, { useEffect } from "react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import authStore from "../../stores/authStore";
+import AddProject from "../AddProject/AddProject";
 import SemesterList from "../SemesterList/SemesterList";
 import SideMenu from "../SideMenu/SideMenu";
 import "./Home.css";
 
 const Home = () => {
-  console.log(authStore.user);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    !authStore.user && navigate("/login");
-  }, [authStore.user]);
-
   return (
     <div className="home">
       <div className="home__side-menu">
         <SideMenu />
       </div>
-      <div className="home__playground">
-        <SemesterList />
+      <div className="home__playground container">
+        <Routes>
+          <Route path="/" element={<SemesterList />} />
+          <Route path="/add-project" element={<AddProject />} />
+        </Routes>
       </div>
     </div>
   );
