b5ad14fd02ba3f673f501438b30ab16487d86cd9
abdullah-alkhareji <outsiderandco@gmail.com>
2022-04-19 12:47:53 +0300

    add ProjectList to semester item

--- src/components/SemesterList/SemesterItem.js
+++ src/components/SemesterList/SemesterItem.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { Accordion } from "react-bootstrap";
+import projectStore from "../../stores/projectStore";
+import ProjectList from "../ProjectList/ProjectList";
 import "./SemesterList.css";
 
 const SemesterItem = ({ semester }) => {
+  // const [projectsList, setProjectsList] = useState(
+  //   projectStore.projects
+  //     .filter((p) => p.semester === semester.id)
+  //     .map((p) => p.name)
+  // );
+
+  // const handleProjectList = () => {
+
+  // }
+
+  // const projectsList = projectStore.projects
+  //   .filter((p) => p.semester === semester.id)
+  //   .map((p) => p.name);
+
   return (
     <Accordion.Item key={semester.id} eventKey={String(semester.id)}>
       <Accordion.Header>{semester.name.toUpperCase()}</Accordion.Header>
       <Accordion.Body>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste, totam.
-        Vitae cum odit nulla temporibus error, eius molestiae alias numquam?
+        <ProjectList semester={semester} />
+        {/* {projectsList.length > 0 ? (
+          projectsList
+        ) : (
+          <h6 className="text-danger">There is no project</h6>
+        )} */}
       </Accordion.Body>
     </Accordion.Item>
   );
